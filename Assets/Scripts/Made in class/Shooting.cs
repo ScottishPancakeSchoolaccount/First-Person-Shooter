43f2accfa906f6010635d9df9634e07e0905e610
ScottishPancakeSchoolaccount <144125195+ScottishPancakeSchoolaccount@users.noreply.github.com>
2023-12-19 14:22:41 +0100

    Forgot to comit!

--- Assets/Scripts/Made in class/Shooting.cs
+++ Assets/Scripts/Made in class/Shooting.cs
@@ -1,3 +1,5 @@
+
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -37,33 +39,3 @@ public class Shooting : MonoBehaviour
 }
 
 
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class Shooting : MonoBehaviour
-//{
-
-//    public Camera cam;
-
-//    private Ray ray;
-//    private RaycastHit hit;
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if (Input.GetMouseButtonDown(0))
-//        {
-//            ray = cam.ScreenPointToRay(Input.mousePosition);
-//            if (Physics.Raycast(ray, out hit))
-//            {
-//                if (hit.collider.tag.Equals("NPC"))
-//                {
-//                    Destroy(hit.collider.gameObject);
-//                    Debug.Log("DIE DIE DIE!");
-//                }
-
-//            }
-//        }
-//    }
-//}
